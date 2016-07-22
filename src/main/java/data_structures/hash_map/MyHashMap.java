5521839695794191fa5157214d45bc8c0e77c9a3
Pustik41 <Pustik41@gmail.com>
2016-07-22 18:48:21 +0300

    DoRemoveHashMap

---
+++ src/main/java/data_structures/hash_map/MyHashMap.java
@@ -0,0 +1,321 @@
+package data_structures.hash_map;
+
+import java.util.*;
+
+/**
+ * Created by dfsdfsddfsdf on 22.07.16.
+ */
+public class MyHashMap<K, V> {
+
+    private static final double DEFAULT_LOAD_FACTOR = 0.75;
+    private static final int DEFAULT_CAPACITY = 16;
+    private MyEntry<K, V>[] table;
+    private double loadFactor;
+    private int threshold;
+    private int capacity;
+    private int size;
+
+    @SuppressWarnings({"rawtypes","unchecked"})
+    public MyHashMap() {
+        loadFactor = DEFAULT_LOAD_FACTOR;
+        capacity = DEFAULT_CAPACITY;
+        table = (MyEntry<K, V>[])new MyEntry[capacity];
+        threshold = (int)(capacity * loadFactor);
+    }
+
+    @SuppressWarnings({"rawtypes","unchecked"})
+    public MyHashMap(int capacity) {
+        this.capacity = capacity;
+        loadFactor = DEFAULT_LOAD_FACTOR;
+
+        table = (MyEntry<K, V>[]) new MyEntry[capacity];
+        threshold = (int)(capacity * loadFactor);
+
+    }
+
+    @SuppressWarnings({"rawtypes","unchecked"})
+    public MyHashMap(double loadFactor, int capacity) {
+        this.loadFactor = loadFactor;
+        this.capacity = capacity;
+        table = (MyEntry<K, V>[])new MyEntry[capacity];
+        threshold = (int)(capacity * loadFactor);
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public boolean containsKey(Object key) {
+        return findByKey(key) != null;
+    }
+
+    public boolean containsValue(Object value) {
+
+        for (int i = 0; i < table.length; i++) {
+
+            if(table[i] == null) continue;
+
+            MyEntry<K, V> iterator = table[i];
+
+            if(value == null){
+
+                while (iterator != null) {
+                    if(iterator.value == null) return true;
+                    iterator = iterator.next;
+                }
+            } else {
+
+                while (iterator != null) {
+                    if(value.equals(iterator.value)) return true;
+                    iterator = iterator.next;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    public V get(Object key) {
+        return findByKey(key).value;
+    }
+
+    private MyEntry<K, V> findByKey(Object key){
+
+        if(key == null){
+
+            MyEntry<K, V> iterator = table[0];
+            while (iterator != null) {
+                if (iterator.key == null) {
+                    return iterator;
+                }
+                iterator = iterator.next;
+            }
+        } else {
+
+            int position = findPosition((K) key);
+
+            MyEntry<K, V> iterator = table[position];
+            while (iterator != null) {
+                if (key.equals(iterator.key)) {
+                    return iterator;
+                }
+                iterator = iterator.next;
+            }
+        }
+
+        return null;
+    }
+
+    public V put(K key, V value) {
+
+        if(key == null){
+
+            if (table[0] == null) {
+                table[0] = new MyEntry<>(key, value);
+            } else {
+                MyEntry<K, V> iterator = table[0];
+                MyEntry<K, V> parent;
+                do {
+                    parent = iterator;
+                    if (iterator.key == null) {
+                        return iterator.setValue(value);
+                    }
+                    iterator = iterator.next;
+                } while (iterator != null);
+                parent.next = new MyEntry<>(key, value);
+            }
+        } else {
+
+            int position = findPosition(key);
+
+            if (table[position] == null) {
+                table[position] = new MyEntry<>(key, value);
+            } else {
+                MyEntry<K, V> iterator = table[position];
+                MyEntry<K, V> parent;
+                do {
+                    parent = iterator;
+                    if (key.equals(iterator.key)) {
+                        return iterator.setValue(value);
+                    }
+                    iterator = iterator.next;
+                } while (iterator != null);
+                parent.next = new MyEntry<>(key, value);
+            }
+        }
+
+        size++;
+        if(size >= threshold){
+            resize();
+        }
+
+        return null;
+    }
+
+    private boolean resize() {
+        @SuppressWarnings({"rawtypes", "unchecked"})
+        MyEntry<K, V>[] resized = (MyEntry<K, V>[])new MyEntry[table.length * 2];
+        threshold = (int)(resized.length * loadFactor);
+        capacity = resized.length;
+
+        return rehash(resized);
+    }
+
+    private boolean rehash(MyEntry<K, V>[] resized) {
+
+        for (int i = 0; i < table.length; i++) {
+            if(table[i] == null) continue;
+            MyEntry<K, V> iterator = table[i];
+            while (iterator != null){
+                putInNewTable(resized, iterator);
+                iterator = iterator.next;
+            }
+        }
+
+        table = resized;
+        return true;
+    }
+
+    private void putInNewTable(MyEntry<K, V>[] resized, MyEntry<K, V> current) {
+
+        MyEntry<K, V> copy = new MyEntry<>(current.getKey(), current.getValue());
+
+        if(current.key == null){
+
+            if(resized[0] == null){
+                resized[0] = copy;
+            } else {
+                MyEntry<K, V> iterator = resized[0];
+                MyEntry<K,V> parent;
+                do {
+                    parent = iterator;
+                    iterator = iterator.next;
+                }  while (iterator != null);
+                parent.next = copy;
+            }
+            return;
+        }
+
+        int position = findPosition(current.key);
+
+        if(resized[position] == null){
+            resized[position] = copy;
+        } else {
+            MyEntry<K, V> iterator = resized[position];
+            MyEntry<K,V> parent;
+            do {
+                parent = iterator;
+                iterator = iterator.next;
+            }  while (iterator != null);
+            parent.next = copy;
+        }
+    }
+
+    private int findPosition(K key) {
+        return Math.abs(key.hashCode()) % capacity;
+    }
+
+    public V remove(Object key) {
+
+        return null;
+    }
+
+    public void putAll(Map<? extends K, ? extends V> m) {
+
+        for (Map.Entry<? extends K, ? extends V> entry : m.entrySet()) {
+            put(entry.getKey(), entry.getValue());
+        }
+
+    }
+
+    public void clear() {
+
+    }
+
+    public Set<K> keySet() {
+
+        Set<K> set = new HashSet<>();
+
+        for (int i = 0; i < table.length; i++) {
+
+            if(table[i] == null) continue;
+
+            MyEntry<K, V> iterator = table[i];
+            while (iterator != null) {
+                set.add(iterator.key);
+                iterator = iterator.next;
+            }
+        }
+
+        return set;
+    }
+
+    public Collection<V> values() {
+
+        Collection<V> collection = new LinkedList<>();
+
+        for (int i = 0; i < table.length; i++) {
+
+            if(table[i] == null) continue;
+
+            MyEntry<K, V> iterator = table[i];
+            while (iterator != null) {
+                collection.add(iterator.value);
+                iterator = iterator.next;
+            }
+        }
+
+        return collection;
+    }
+
+    public Set<MyEntry<K, V>> entrySet() {
+
+        Set<MyEntry<K, V>> set = new HashSet<>();
+
+        for (int i = 0; i < table.length; i++) {
+
+            if(table[i] == null) continue;
+
+            MyEntry<K, V> iterator = table[i];
+            while (iterator != null) {
+                set.add(iterator);
+                iterator = iterator.next;
+            }
+        }
+
+        return set;
+    }
+
+    public static class MyEntry<K, V>{
+
+        public K key;
+        public V value;
+        public MyEntry<K, V> next;
+
+        public MyEntry() {
+        }
+
+        public MyEntry(K key, V value) {
+            this.key = key;
+            this.value = value;
+        }
+
+        public K getKey() {
+            return key;
+        }
+
+        public V getValue() {
+            return value;
+        }
+
+        public V setValue(V value) {
+            V forReturn = this.value;
+            this.value = value;
+            return forReturn;
+        }
+    }
+}
---
+++ src/test/main/java/data_structures/hash_map/MyHashMapTest.java
@@ -0,0 +1,134 @@
+package data_structures.hash_map;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by dfsdfsddfsdf on 22.07.16.
+ */
+public class MyHashMapTest {
+
+    MyHashMap<Integer, String> map;
+
+    @Before
+    public void setUp() {
+
+        map = new MyHashMap<>(3);
+        map.put(6, "A");
+        map.put(21, "B");
+        map.put(null, "Z");
+        map.put(18, "C");
+        map.put(42, "D");
+    }
+
+    @Test
+    public void testSize() {
+        assertEquals(5, map.size());
+    }
+
+    @Test
+    public void testIsEmpty() {
+        assertFalse(map.isEmpty());
+    }
+
+    @Test
+    public void testContainsKey_true() {
+        assertTrue(map.containsKey(21));
+    }
+
+    @Test
+    public void testContainsKey_false() {
+        assertFalse(map.containsKey(41));
+    }
+
+    @Test
+    public void testContainsKey_when_key_null() {
+        assertTrue(map.containsKey(null));
+    }
+
+    @Test
+    public void testContainsValue_true() {
+        assertTrue(map.containsValue("C"));
+    }
+
+    @Test
+    public void testContainsValue_false() {
+        assertFalse(map.containsValue("W"));
+    }
+
+    @Test
+    public void testContainsValue_when_value_null() {
+        map.put(13, null);
+        assertTrue(map.containsValue(null));
+    }
+
+    @Test
+    public void testPut_when_same_key() {
+        assertEquals("C", map.put(18, "H"));
+    }
+
+    @Test
+    public void testPut_when_same_key_null() {
+        assertEquals("Z", map.put(null, "L"));
+    }
+
+    @Test
+    public void testGet_by_key() {
+        assertEquals("B", map.get(21));
+    }
+
+    @Test
+    public void testGet_by_key_null() {
+        assertEquals("Z", map.get(null));
+    }
+
+    @Test
+    public void testEntrySet() {
+        Set<MyHashMap.MyEntry<Integer, String>> set = map.entrySet();
+        assertEquals(map.size(), set.size());
+    }
+
+    @Test
+    public void testKeySet() {
+        Set<Integer> set = map.keySet();
+        for (Integer integer : set) {
+            System.out.println(integer);
+        }
+        assertEquals(map.size(), set.size());
+    }
+
+    @Test
+    public void testValues() {
+        Collection<String> collection = map.values();
+        System.out.println(collection);
+        assertEquals(map.size(), collection.size());
+    }
+
+    @Test
+    public void testPutAll() {
+        Map<Integer, String> mapTest = new HashMap<>();
+        map.put(32, "D");
+        map.put(64, "J");
+        map.put(71, "Q");
+        int size = map.size() + mapTest.size();
+        map.putAll(mapTest);
+        assertEquals(size, map.size());
+    }
+
+    @Test
+    public void testPutAll_same_key() {
+        Map<Integer, String> mapTest = new HashMap<>();
+        map.put(32, "D");
+        map.put(18, "J");
+        map.put(71, "Q");
+        map.putAll(mapTest);
+        assertFalse(map.containsValue("C"));
+    }
+}
